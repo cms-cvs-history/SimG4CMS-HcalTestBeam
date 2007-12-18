5a01bfc5d5af811fdd085caea18c437dccffee80
Sunanda Banerjee <sha1-b4788d7bc76f55c45b7ae35f27b935e0e0f10682@cern.ch>
2007-12-18 22:06:30 +0000

    Make a workable solution for 2007 TB simulation

--- src/HcalTB06Analysis.cc
+++ src/HcalTB06Analysis.cc
@@ -4,11 +4,11 @@
 // Class  :     HcalTB06Analysis
 //
 // Implementation:
-//     Main analysis class for Hcal Test Beam 2004 Analysis
+//     Main analysis class for Hcal Test Beam 2006 Analysis
 //
 // Original Author:
 //         Created:  Tue May 16 10:14:34 CEST 2006
-// $Id: HcalTB06Analysis.cc,v 1.2 2006/11/13 10:32:15 sunanda Exp $
+// $Id: HcalTB06Analysis.cc,v 1.3 2007/03/08 00:19:50 sunanda Exp $
 //
   
 // system include files
@@ -66,7 +66,7 @@ HcalTB06Analysis::HcalTB06Analysis(const edm::ParameterSet &p): histo(0) {
   beamline_RM->rotateZ(-beamPhi);
   beamline_RM->rotateY(-beamThet);
  
-  edm::LogInfo("HcalTBSim") << "HcalTB04:: Initialised as observer of BeginOf"
+  edm::LogInfo("HcalTBSim") << "HcalTB06:: Initialised as observer of BeginOf"
 			    << "Job/BeginOfRun/BeginOfEvent/G4Step/EndOfEvent"
 			    << " with Parameter values:\n \tbeamOffset = " 
 			    << beamOffset << "\ticeta = " << iceta 
